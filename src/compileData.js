5166959f5f2a2eed117a72ecfadfe8a808e548a3
Anthony Frasso <afrasso@cimpress.com>
2022-01-29 21:23:16 -0500

    Fixing build.

--- src/compileData.js
+++ src/compileData.js
@@ -6,7 +6,9 @@ const DATA_DIR = "./data";
 const COMPILED_DATA_DIR = "./compiledData";
 
 const files = fs.readdirSync(DATA_DIR);
-fs.mkdirSync(COMPILED_DATA_DIR);
+if (!fs.existsSync(COMPILED_DATA_DIR)) {
+  fs.mkdirSync(COMPILED_DATA_DIR);
+}
 for (const file of files) {
   const data = yaml.load(fs.readFileSync(path.join(DATA_DIR, file)));
   fs.writeFileSync(
